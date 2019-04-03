12dea8203b2fb51e339ecfff45e48b9b665c74b3
thelazarbeam <eddie.lazar@yahoo.com>
2019-04-03 11:07:54 -0400

    added documentation comments for all existing methods

--- app.py
+++ app.py
@@ -1,19 +1,22 @@
 from flask import Flask, request, Response, render_template
 
-
 app = Flask(__name__)
 
 @app.route("/")
 def splash():
+	"""main page. Loads up splash screen."""
 	return render_template("splash.html")
 
 @app.route("/gamepage")
 def gamepage():
+	"""The webpage for being in a game. Loads up game HTML elements.
+	Should be unable to reach here manually."""
 	return render_template("gamepage.html")
 
 @app.route("/piledrop", methods=["POST"])
 def piledrop():
-
+	"""handler for placing a card on a pile.
+	called with AJAX from game page. cannot manually get here."""
 	color = request.get_data().decode("utf-8")
 
 	resp = Response("Hello from localhost 5k! You dragged a card to the " + color + " pile!");
