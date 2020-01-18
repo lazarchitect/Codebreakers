e0fd307516e3fc5a9b3108298652ea2dd111c65d
Eddie <eddie.lazar@yahoo.com>
2020-01-18 03:56:13 -0500

    merging styled code with functional

---
+++ static/scripts/manuscript.js
@@ -0,0 +1,34 @@
+function notify() {
+    alert("So... you have chosen... to sign up");
+}
+
+var waitingUsers = ["Eddie", "Sean", "Chris", "Mike"];
+
+
+function fillWaitingRoom(){
+
+    var waitingRoom = document.getElementById("waitingRoom");
+
+    var i;
+    for(i = 0; i < waitingUsers.length; i++){
+
+        var line = document.createElement("div");
+        line.setAttribute("class", "openGame");
+        if(i%2==0){
+            line.style.backgroundColor = "skyblue";
+        }
+        else{
+            line.style.backgroundColor = "white";
+        }
+        line.setAttribute("class", "openGame");
+        var anchor = document.createElement("a");
+        anchor.setAttribute("href", "google.com");
+        anchor.innerText = waitingUsers[i];
+
+        line.appendChild(anchor);
+
+        waitingRoom.appendChild(line);
+
+        // waitingRoom.appendChild(document.createElement("br"));
+    }
+}
